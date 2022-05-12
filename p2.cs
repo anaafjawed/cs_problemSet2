28c16d402c01a3357e28634f090a91ed34f1418d
Anaaf Javed <anaaf.javed@logiciel.com>
2022-05-12 19:26:52 +0500

    p4, p2 of problem set 1

---
+++ p2.cs
@@ -0,0 +1,79 @@
+
+using System.Collections.Generic;
+namespace ProblemSet2
+{
+    class P2
+    {
+        public static void Main(string[] args)
+        {
+            solve();
+        }
+
+        public static void solve()
+        {
+
+            string file = @"C:\Users\anaaf.javed\ProblemSet2\number.txt";
+
+            string[] lines = File.ReadAllLines(file);
+            int t = 0;
+            int test = int.Parse(lines[t]);
+            t++;
+
+            for (int i = 0; i < test; i++)
+            {
+                string[] line = lines[t].Split(" ");
+                t++;
+               int source = int.Parse(line[0]);
+               int target = int.Parse(line[1]);
+               int num = int.Parse(line[2]);
+
+            int temp = toDecimal(source , num);
+            int res = decimalToTarget(target, temp);
+            System.Console.WriteLine(res);
+            
+            }
+
+        
+        }
+
+
+        public static int toDecimal(int source,  int num) {
+
+            string str = num.ToString();
+            int size = str.Length - 1;
+            int res = 0;
+            int i = 0;
+            while(size >= 0) {
+            int r =  (int)Char.GetNumericValue(str[i]);;
+            res = res + ((r) * (int) Math.Pow(source, size));
+             size --;
+             i++;
+            }     
+            return res;
+        }
+
+        public static int decimalToTarget(int target,  int decimalVersion) {
+        
+            Stack<int> res = new Stack<int>();
+
+            int num = decimalVersion;
+
+            while(num >= 1) {
+                int rem = num % target;
+                res.Push(rem);
+                num = num / target;
+            }
+
+            string outp = "";
+            
+           while(res.Count() > 0) {
+                int pop = res.Pop();
+                outp = outp + pop.ToString();
+            }
+
+           
+
+            return int.Parse(outp);
+        }
+    }
+}
---
+++ p4.cs
@@ -0,0 +1,70 @@
+
+using System;
+using System.Collections;
+namespace ProblemSet2
+{
+    class P4
+    {
+        public static void Main(string[] args)
+        {
+            solve();
+        }
+
+        public static void solve()
+        {
+
+            string file = @"C:\Users\anaaf.javed\ProblemSet2\virtualfriends.txt";
+
+            string[] lines = File.ReadAllLines(file);
+            int t = 0;
+            int test = Int32.Parse(lines[t]);
+            t++;
+
+            for (int i = 0; i < test; i++)
+            {
+                int friendships_count = Int32.Parse(lines[t]);
+                t++;
+
+                string[] friends = new string[friendships_count];
+
+                // storing friendship
+
+                for(int j = 0; j < friendships_count; j++) {
+                    friends[j] = lines[t];
+                    t++;
+                }
+                
+                
+                ArrayList network = new ArrayList();
+                
+                network.Add(new HashSet<String>());
+
+                for(int j = 0; j < friends.Length; j++){
+                    string[] temp = friends[j].Split(" ");
+                    Boolean isFound = false;
+                    foreach (HashSet<String> net in network ) {
+                        HashSet<String> hs = net;
+                        if(hs.Contains(temp[0])) {
+                            hs.Add(temp[1]);
+                            System.Console.WriteLine(hs.Count);
+                            isFound = true;
+                        } else if(hs.Contains(temp[1])) {
+                             hs.Add(temp[0]);
+                            System.Console.WriteLine(hs.Count);
+                            isFound = true;
+                        }
+                    }
+
+                    if(!isFound) {
+                        HashSet<String> newHs = new HashSet<String>();
+                        newHs.Add(temp[0]);
+                        newHs.Add(temp[1]);
+                        network.Add(newHs);
+                        System.Console.WriteLine(newHs.Count);
+                    }
+                }
+               
+            }
+        }
+    }
+}
